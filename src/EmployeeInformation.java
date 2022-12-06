ac2472017702e86de0c14edf5f6cf54079452058
dbhor <dbhor1000@gmail.com>
2022-12-06 18:49:34 +0300

    commit, branch, pre-final version(2) (корректировка с учётом того, что массив может быть не заполнен)

--- src/EmployeeInformation.java
+++ src/EmployeeInformation.java
@@ -5,338 +5,178 @@ public class EmployeeInformation {
 
 
 
-    public static void printEmployees(Employee[] arr) {
+    private static int employeeCountService(Employee[] arr){     //Считаем сотрудников в книге, служебный метод
 
-        System.out.println("Список всех сотрудников: ");
+        int employeeCount = 0;
 
         for (int i = 0; i < arr.length; i++) {
-            System.out.println((arr[i].toString()));
+
+            if (arr[i].getName() != ("0"))
+
+                employeeCount++;
+        }
+
+        return(employeeCount);
+    }
+
+
+    public static void printEmployees(Employee[] arr) {
+
+        if (employeeCountService(arr) != 0) {
+
+            System.out.println("Список всех сотрудников: ");
+
+            for (int i = 0; i < arr.length; i++) {
+
+                if (arr[i].getName() != ("0")) {
+
+                    System.out.println((arr[i].toString() + " " + arr[i].getId()));
+                }
+            }
+
+        } else {
+
+            System.out.println("В книге не указан ни один сотрудник");
+
         }
 
         System.out.println();
     }
 
 
+
     public static void calculateSalaryExpenses(Employee[] arr) {
 
-        int totalSalary = 0;
+        if (employeeCountService(arr) != 0) {
 
-        for (int i = 0; i < arr.length; i++) {
+            int totalSalary = 0;
 
-            totalSalary = totalSalary + arr[i].getSalary();
+            for (int i = 0; i < arr.length; i++) {
+
+                if (arr[i].getName() != ("0")) {
+
+                    totalSalary = totalSalary + arr[i].getSalary();
+                }
+            }
+
+            System.out.println("Общие расходы на зарплату составляют: " + totalSalary);
+            System.out.println();
+
+        } else {
+
+            System.out.println("В книге не указан ни один сотрудник");
+
         }
-
-        System.out.println("Общие расходы на зарплату составляют: " + totalSalary);
-        System.out.println();
 
     }
 
     public static void findMinSalary(Employee[] arr) {
 
-        Employee[] arrSorted = Arrays.copyOf(arr, arr.length);
+        if (employeeCountService(arr) != 0) {
 
-        Arrays.sort(arrSorted, Comparator.comparing(Employee::getSalary));
+            int employeesInBook = 0;
 
-        System.out.println("Сотрудник с минимальной зарплатой: " + arrSorted[0].toString());
 
-        System.out.println();
+            for (int i = 0; i < arr.length; i++) {          //выясняем, сколько сотрудников в книге не указано
+
+                if (arr[i].getName() == ("0")) {
+
+                    employeesInBook++;
+
+                }
+            }
+
+
+
+            Employee[] arrSorted = Arrays.copyOf(arr, arr.length);
+
+            Arrays.sort(arrSorted, Comparator.comparing(Employee::getSalary));
+
+            System.out.println("Сотрудник с минимальной зарплатой: " + arrSorted[employeesInBook].toString());
+
+            System.out.println();
+
+        } else {
+
+            System.out.println("В книге не указан ни один сотрудник");
+
+        }
     }
 
     public static void findMaxSalary(Employee[] arr) {
 
-        Employee[] arrSorted = Arrays.copyOf(arr, arr.length);
+        if (employeeCountService(arr) != 0) {
 
-        Arrays.sort(arrSorted, Comparator.comparing(Employee::getSalary));
+            Employee[] arrSorted = Arrays.copyOf(arr, arr.length);
 
-        System.out.println("Сотрудник с максимальной зарплатой: " + arrSorted[(arrSorted.length - 1)].toString());
+            Arrays.sort(arrSorted, Comparator.comparing(Employee::getSalary));
 
-        System.out.println();
+            System.out.println("Сотрудник с максимальной зарплатой: " + arrSorted[(arrSorted.length - 1)].toString());
+
+            System.out.println();
+
+        } else {
+
+            System.out.println("В книге не указан ни один сотрудник");
+
+        }
     }
+
 
     public static void calculateAverageSalary(Employee[] arr) {
 
-        int totalSalary = 0;
+        if (employeeCountService(arr) != 0) {
 
-        for (int i = 0; i < arr.length; i++) {
+            int totalSalary = 0;
+            int employeesInBook = 0;
 
-            totalSalary = totalSalary + arr[i].getSalary();
+            for (int i = 0; i < arr.length; i++) {          //выясняем количество сотрудников
+
+                if (arr[i].getName() != ("0")) {
+
+                    employeesInBook++;
+
+                }
+            }
+
+            for (int i = 0; i < arr.length; i++) {
+
+                totalSalary = totalSalary + arr[i].getSalary();
+            }
+
+            System.out.println("Средняя зарплата составляет: " + (totalSalary / employeesInBook));
+            System.out.println();
+
+        } else {
+
+            System.out.println("В книге не указан ни один сотрудник");
+
         }
-
-        System.out.println("Средняя зарплата составляет: " + (totalSalary/arr.length) );
-        System.out.println();
     }
+
 
     public static void printEmployeeNames(Employee[] arr) {
 
-        System.out.println("Список имён всех сотрудников: ");
+        if (employeeCountService(arr) != 0) {
 
-        for (int i = 0; i < arr.length; i++) {
-
-            System.out.println(arr[i].getName());
-        }
-
-        System.out.println();
-
-    }
-
-    public static void indexSalary(Employee[] arr, int change) {
-
-        System.out.println("Зарплата всех сотрудников после индексации на " + change + " процентов :");
-
-        for (int i = 0; i < arr.length; i++) {
-
-            arr[i].setSalary( (arr[i].getSalary() * (100 + change)) / 100 );
-            System.out.println(arr[i].toString());
-
-        }
-        System.out.println();
-    }
-
-    public static void findDepartmentMinSalary(Employee[] arr, int numberOfDepartment) {
-
-        Employee[] arrSorted = Arrays.copyOf(arr, arr.length);
-
-        Arrays.sort(arrSorted, Comparator.comparing(Employee::getSalary));
-        int employeeFound = 0;
-
-
-        for (int i = 0; i < arr.length; i++) {
-
-            if (arrSorted[i].getDepartment() == numberOfDepartment) {
-
-                System.out.println("Сотрудник с минимальной зарплатой в отделе " + numberOfDepartment + " - " + arrSorted[i].toString());
-                employeeFound = employeeFound + 1;
-                break;
-            }
-        }
-
-        if (employeeFound == 0) {
-
-            System.out.println("В указанном департаменте нет сотрудников");
-        }
-        System.out.println();
-    }
-
-    public static void findDepartmentMaxSalary(Employee[] arr, int numberOfDepartment) {
-
-        Employee[] arrSorted = Arrays.copyOf(arr, arr.length);
-
-        Arrays.sort(arrSorted, Comparator.comparing(Employee::getSalary));
-        int employeeFound = 0;
-
-
-        for (int i = 0; i < arr.length; i++) {
-
-            if (arrSorted[i].getDepartment() == numberOfDepartment) {
-
-                System.out.println("Сотрудник с максимальной зарплатой в отделе " + numberOfDepartment + " - " + arrSorted[arrSorted.length - 1].toString());
-                employeeFound = employeeFound + 1;
-                break;
-            }
-        }
-
-        if (employeeFound == 0) {
-
-            System.out.println("В указанном департаменте нет сотрудников");
-        }
-        System.out.println();
-    }
-
-
-    public static void calculateDepartmentSalary(Employee[] arr, int numberOfDepartment) {
-
-        int employeeFound = 0;
-        int totalSalary = 0;
-
-
-        for (int i = 0; i < arr.length; i++) {
-
-            if (arr[i].getDepartment() == numberOfDepartment) {
-
-                totalSalary = totalSalary + arr[i].getSalary();
-                employeeFound++;
-
-            }
-        }
-
-        if (employeeFound == 0) {
-
-            System.out.println("В указанном департаменте нет сотрудников");
-        } else {
-
-            System.out.println("Суммарная затрата для отдела " + numberOfDepartment + " равняется " + totalSalary);
-        }
-
-        System.out.println();
-    }
-
-
-    public static void calculateDepartmentAverageEmployeeSalary(Employee[] arr, int numberOfDepartment) {
-
-        int employeeFound = 0;
-        int totalSalary = 0;
-
-
-        for (int i = 0; i < arr.length; i++) {
-
-            if (arr[i].getDepartment() == numberOfDepartment) {
-
-                totalSalary = totalSalary + arr[i].getSalary();
-                employeeFound++;
-
-            }
-        }
-
-        if (employeeFound == 0) {
-
-            System.out.println("В указанном департаменте нет сотрудников");
-        } else {
-
-            System.out.println("Средняя зарплата для отдела " + numberOfDepartment + " равняется " + totalSalary/employeeFound);
-        }
-
-        System.out.println();
-    }
-
-    public static void indexDepartmentEmployeeSalary(Employee[] arr, int numberOfDepartment, int changeSalary) {
-
-        int employeeFound = 0;
-
-
-        for (int i = 0; i < arr.length; i++) {
-
-            if (arr[i].getDepartment() == numberOfDepartment) {
-
-                arr[i].setSalary((arr[i].getSalary() * (100 + changeSalary))/100);
-                employeeFound++;
-
-            }
-        }
-
-        if (employeeFound == 0) {
-
-            System.out.println("В указанном департаменте нет сотрудников");
-        } else {
-
-            System.out.println("После индексации на " + changeSalary + " процентов, зарплаты в отделе " + numberOfDepartment + " составляют: ");
+            System.out.println("Список имён всех сотрудников: ");
 
             for (int i = 0; i < arr.length; i++) {
 
-                if (arr[i].getDepartment() == numberOfDepartment) {
+                if (arr[i].getName() != ("0")) {
 
-                    System.out.println(arr[i].toString());
+                    System.out.println(arr[i].getName());
                 }
             }
 
-        }
-
-        System.out.println();
-    }
-
-
-    public static void printDepartmentEmployeeNameSalary(Employee[] arr, int numberOfDepartment) {
-
-        int employeeFound = 0;
-
-        for (int i = 0; i < arr.length; i++) {
-
-            if (arr[i].getDepartment() == numberOfDepartment) {
-
-                employeeFound++;
-            }
-        }
-
-        if (employeeFound == 0) {
-
-            System.out.println("В указанном департаменте нет сотрудников");
+            System.out.println();
 
         } else {
 
-            System.out.println("В " + numberOfDepartment + " отделе работают следующие сотрудники: ");
+            System.out.println("В книге не указан ни один сотрудник");
 
-            for (int i = 0; i < arr.length; i++) {
-
-                if (arr[i].getDepartment() == numberOfDepartment) {
-
-                    System.out.println(arr[i].getName() + " " + arr[i].getSalary());
-
-                }
-            }
         }
-        System.out.println();
+
     }
-
-    public static void printBySalaryHigher(Employee[] arr, int salary) {
-
-        int employeeFound = 0;
-
-        for (int i = 0; i < arr.length; i++) {
-
-            if (arr[i].getSalary() >= salary ) {
-
-                employeeFound++;
-
-            }
-        }
-
-        if (employeeFound == 0) {
-
-            System.out.println("Нет сотрудников с зарплатой, выше или равной указанной.");
-
-        } else {
-
-            System.out.println("Зарплату выше или равную " + salary + " имеют следующие сотрудники: ");
-
-            for (int i = 0; i < arr.length; i++) {
-
-                if (arr[i].getSalary() >= salary ) {
-
-                    System.out.println(arr[i].toString());
-
-                }
-            }
-
-        }
-        System.out.println();
-    }
-
-
-    public static void printBySalaryLower(Employee[] arr, int salary) {
-
-        int employeeFound = 0;
-
-        for (int i = 0; i < arr.length; i++) {
-
-            if (arr[i].getSalary() <= salary ) {
-
-                employeeFound++;
-
-            }
-        }
-
-        if (employeeFound == 0) {
-
-            System.out.println("Нет сотрудников с зарплатой, ниже или равной указанной.");
-
-        } else {
-
-            System.out.println("Зарплату ниже или равную " + salary + " имеют следующие сотрудники: ");
-
-            for (int i = 0; i < arr.length; i++) {
-
-                if (arr[i].getSalary() <= salary ) {
-
-                    System.out.println(arr[i].toString());
-
-                }
-            }
-
-        }
-
-        System.out.println();
-    }
-
-
 
 }
-
